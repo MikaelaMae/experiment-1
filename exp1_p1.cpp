40d751e65b76d1f44d80e25eab78c39d2900a547
MikaelaMae <51978118+MikaelaMae@users.noreply.github.com>
2019-07-07 23:00:55 +0800

    Add files via upload

---
+++ exp1_p1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+	double a, b, c, d;
+
+	cout << "Ticket Price       " << endl;
+	cout << "Box: $250" << endl;
+	cout << "Sideline: $100" << endl;
+	cout << "Premium: $50" << endl;
+	cout << "General Admission: $25" << endl;
+	cout << "Enter number of tickets sold for Box: " << endl;
+	cin >> a;
+	cout << "Enter number of tickets sold for Sideline: " << endl;
+	cin >> b;
+	cout << "Enter number of tickets sold for Premium: " << endl;
+	cin >> c;
+	cout << "Enter number of tickets sold for General Admission: " << endl;
+	cin >> d;
+
+	cout << "Total Ticket Price: " << endl;
+	cout << "For Box: " << setprecision(2) << fixed << 250*a << endl;
+	cout << "For Sideline: " << setprecision(2) << 100*b << endl;
+	cout << "For Premium: " << 50 * c << endl;
+	cout << "For General Admission: " << d * 25 << endl;
+	_getch();
+	return 0;
+}
---
+++ exp1_p2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double mass, density;
+	cout << "Input Mass: ";
+	cin >> mass;
+	cout << "Mass = " << mass << "g" << endl << endl;
+	cout << "Input Density: ";
+	cin >> density;
+	cout << "Density = " << density << "g/cm^3" << endl << endl;
+	cout << "Volume of the Object = " << setprecision(2) << mass/(4*density) << "cm^3" << endl;
+	_getch();
+	return 0;
+}
